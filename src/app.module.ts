ed4cc4c4bcb81fe3f1660872017ab1a9c6d25451
NieladVerse <nieladverse@gmail.com>
2024-11-17 21:34:37 -0500

    [DEV] - adding .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    MongooseModule.forRoot('mongodb+srv://users_admin:egmrZUxh9kGVntjY@cluster0.wok38ui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {}),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'], // Hace que las variables estén disponibles globalmente
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DBUSER}:${process.env.DB_PASS}@cluster0.wok38ui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {}),
   ],
   controllers: [AppController],
   providers: [AppService],
